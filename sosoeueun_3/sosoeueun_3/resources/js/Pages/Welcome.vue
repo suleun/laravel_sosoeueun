91a6373626b6ab4ccafe6ceba68c5dd57ab85c69
suleun <teaaa030@gmail.com>
2021-12-09 11:56:05 +0900

    plus main background image

--- sosoeueun_3/sosoeueun_3/resources/js/Pages/Welcome.vue
+++ sosoeueun_3/sosoeueun_3/resources/js/Pages/Welcome.vue
@@ -15,12 +15,15 @@
                 <Link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 underline">
                     Register
                 </Link>
-            </template>
 
-            
+
+            </template>
         </div>
 
-        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+        <div class="mx-auto sm:px-6 lg:px-8">
+
+                <img src="/storage/images/main.png" alt="">            
+
         </div>
     </div>
 </template>
